3b67767cdfe2512afd5234820fed66739016cd56
Chandrasekhar Samanta <56759909+chandrasekhar2039@users.noreply.github.com>
2025-09-11 22:38:32 +0530

    Update users.js

--- Fullstack-app/backend/routes/users.js
+++ Fullstack-app/backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json([{ id: 1, name: "Rakesh" }, { id: 2, name: "John Doe" }, { id: 3, name: "John Keneddy" }]);
+  res.json([{ id: 1, name: "Rakesh" }, { id: 2, name: "John Doe" }, { id: 3, name: "John Keneddy" }, { id: 4, name: "Max Verstappen" }]);
 });
 
 module.exports = router;
